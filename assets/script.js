ec314ef8855a0ddf5c631f0a3caee0c2a341a05c
Grant Mitchell <grantmitchell@Grants-MacBook-Pro.local>
2023-10-02 16:03:46 -0500

    work completed before tutor assitance

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,62 @@
-// Assignment Code
+// 1. Ask the user all questions
+// 2. Global variables for all the user params for the password
+// 3. We have to get together a list of all allowed characters in the final password
+// 4. Combine all teh allowed possible chatacters into a single array (array merging)
+  // example array merge: var finalArrary = [...specialCharacters, ...lowerCaseLetters]
+// 5. Generate one random character x number of times (for loop) add to string value (var finalPassword)
+  // a. Generate a random index vlaue for each of these characters
+  // b. Random idx value = 0 to array length - 1
+
+var generateBtn = document.querySelector("#generate");
+
+var numberOfCharacters;
+var useNumbers;
+var useSpecialCharacters;
+var useUpperCase;
+var useLowerCase;
+
+var finalPassword = "";
+
+var SpecialCharacters = ["$", "#", "@", "?"];
+var numbers = []
+var lowerCaseLetters = []
+var useUpperCase = []
+var allFinalCharacters = []
+
+
+function askQuestions() {
+
+}
+
+function getAllAllowedCharacters() { 
+
+  return "grant123"
+}
+
 
 // Function to generate a random password: 
 // Instruction 1
-  function generatePassword() {
+function generatePassword() {
     // Prompt for password length
   var length = parseInt(prompt("Enter Password Length (at least 8 characters"));
-  }
 
 // Validate Length
   if (isNaN(length) || length <8 || length > 128) {
-    alert("Please enter a valid password length.");
+    alert("Please enter a valid password length."); 
     return;
   }
 
+
+  getSpecialCharacters();
+
+
+}
 // Prompt for character types
 // Confirms return boolean value
+function getSpecialCharacters() {
+useSpecialCharacters= confirm ("Do you want to use special characters?")
+}
 
-
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
