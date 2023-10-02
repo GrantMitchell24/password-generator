41977bffcf603388ed6812268e34ef32859b5d56
Grant Mitchell <grantmitchell@Grants-MacBook-Pro.local>
2023-10-02 09:52:46 -0500

    initial upload

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
 // Assignment Code
+
+// Function to generate a random password: 
+// Instruction 1
+  function generatePassword() {
+    // Prompt for password length
+  var length = parseInt(prompt("Enter Password Length (at least 8 characters"));
+  }
+
+// Validate Length
+  if (isNaN(length) || length <8 || length > 128) {
+    alert("Please enter a valid password length.");
+    return;
+  }
+
+// Prompt for character types
+// Confirms return boolean value
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
